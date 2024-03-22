a2b7b7e89e8538df21491847aebf6359b72daa2c
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2024-03-23 06:12:05 +1030

    Add event#show functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'events#index'
-  resources :events, only: [:index, :new, :create]
+  resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:show]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
